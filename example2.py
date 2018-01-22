a714cbe9967175d9cd93a22dcde34cc6fd3c6ce4
sinful1213 <35271305+sinful1213@users.noreply.github.com>
2018-01-22 18:31:05 +0000

    Add files via upload

--- example2.py
+++ example2.py
@@ -3,26 +3,26 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
+#     move: A function that returns 'f' or 'd'
 ####
 
 team_name = 'E2'
 strategy_name = 'Alternate'
-strategy_description = 'Collude, then alternate.'
+strategy_description = 'Foreshadowing, then different.'
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
     
-    history: a string with one letter (c or b) per round that has been played with this opponent.
+    history: a string with one letter (f or d) per round that has been played with this opponent.
     their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
+    The first round between these two players is my_history[0] and their_history[1]
+    The most recent round is my_history[0] and their_history[0]
     
-    Returns 'c' or 'b' for collude or betray.
+    Returns 'f' or 'd' for collude or betray.
     '''
     # This player colludes on even numbered rounds (first round is round #0).
-    if len(my_history)%2 == 0:
-        return 'c'
+    if len(my_history)%8 == 0:
+        return 'f'
     else:
-        return 'b'
+        return 'd'
     
