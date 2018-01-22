e41688e50b694e1436a45eb314a23a9fd497e658
csllama <35372760+csllama@users.noreply.github.com>
2018-01-22 13:09:39 -0500

    Add files via upload

--- example3.py
+++ example3.py
@@ -22,9 +22,4 @@ def move(my_history, their_history, my_score, their_score):
     
     Returns 'c' or 'b' for collude or betray.
     '''
-    if len(my_history)==0: # It's the first round; collude.
-        return 'c'
-    elif my_history[-1]=='c' and their_history[-1]=='b':
-        return 'b' # Betray if they were severely punished last time,
-    else:
-        return 'c' # otherwise collude.
+    return 'b'  # This is my super simple change to be added
