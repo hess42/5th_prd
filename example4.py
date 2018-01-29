70cc2d7d7e08325dcba2f047644f4857e6c90764
Michelle <35271238+michelle1270@users.noreply.github.com>
2018-01-29 13:29:38 -0500

    Delete example4.py

--- example4.py
+++
@@ -1,9 +0,0 @@
-def ipd4(my_score, enemy_score, my_history, enemy_history):  
-    if my_score - enemy_score >=2000:
-        collude()
-    else:
-        betray()
-def collude():
-    print 'c'
-def betray():
-    print 'b'
